6515a344bc96ef30bdcf813f7d840a2eb0a8a35e
Jhonny Yamaniha Ytokazu <jyamanih@pe.ibm.com>
2020-05-28 11:13:48 -0500

    Se corrigio path terraform

--- install_terraform.sh
+++ install_terraform.sh
@@ -2,5 +2,5 @@
 # sudo yum install wget -y
 export TERRAFORM_VERSION=0.11.14
 wget  https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d usr/bin/local
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d usr/local
 ln -s /usr/local/terraform /usr/bin/terraform
